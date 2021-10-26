366f9a4f3a89f88455d29aedbee50340fba3508f
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-10-26 09:17:27 -0700

    Clean up Methods schema and preview strucutre

--- studio/schemas/documents/method.js
+++ studio/schemas/documents/method.js
@@ -1,44 +1,30 @@
-import { RiGitCommitLine, RiBubbleChartFill } from 'react-icons/ri'
+import React from 'react'
+import { Link } from "gatsby"
+import PortableText from "./portableText"
+import SanityImage from "gatsby-plugin-sanity-image"
 
-export default {
-  name: 'method',
-  type: 'document',
-  icon: RiBubbleChartFill,
-  title: 'Methods',
-  fields: [
-    {
-      name: 'title',
-      type: 'string',
-      title: 'Title'
-    },
-    {
-      name: 'slug',
-      type: 'slug',
-      title: 'Slug',
-      validation: Rule => Rule.required(),
-      options: {
-        source: 'title',
-        maxLength: 96
+const Method = ({ data }) => {
+  return (
+    <div>
+      <h2>{data.title}</h2>
+      {/*<p>{data.metaDescription}</p>*/}
+      {data.heroImage &&
+        <SanityImage {...data.heroImage} width={500} alt=""
+          style={{
+          width: "100%",
+          objectFit: "cover",
+        }}/>
       }
-    },
-    {
-      name: 'testImage',
-      type: 'image',
-      options: {
-        hotspot: true
-      }
-    },
-    {
-      name: 'metaDescription',
-      type: 'text',
-      title: 'Short Description',
-      rows: 3,
-      validation: Rule => Rule.required()
-    },
-    {
-      name: 'heroImage',
-      type: 'heroImage',
-      title: 'Hero Image'
+      <PortableText blocks={data.overview} />
+      <PortableText blocks={data.steps} />
+      <Link to='/'>Back to Index</Link>
+      {/*<pre>{JSON.stringify(data, null, 2)}</pre>*/}
+    </div>
+  );
+}
+
+export default Method
+Image'
     },
     {
       name: 'disciplinesReference',
--- web/src/components/method.js
+++ web/src/components/method.js
@@ -18,10 +18,55 @@ const Method = ({ data }) => {
       <PortableText blocks={data.overview} />
       <PortableText blocks={data.steps} />
       <Link to='/'>Back to Index</Link>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      {/*<pre>{JSON.stringify(data.heroImage, null, 2)}</pre>*/}
+      {/*<pre>{JSON.stringify(data, null, 2)}</pre>*/}
     </div>
   );
 }
 
 export default Method
+Image'
+    },
+    {
+      name: 'disciplinesReference',
+      type: 'array',
+      title: 'Discipline(s)',
+      description: 'List the disciplines in which this method is most commonly used.',
+      of: [{ type: 'referencedDiscipline'}]
+    },
+    {
+      name: 'overview',
+      type: 'bodyPortableText',
+      title: 'Method Overview'
+    },
+    {
+      name: 'steps',
+      type: 'bodyPortableText',
+      title: 'Method Steps'
+    },
+    {
+      name: 'transputReference',
+      type: 'transputReference',
+      title: 'Input/Output'
+    }
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      thumb: 'heroImage',
+      discipline0: 'disciplinesReference.0.title',
+      discipline1: 'disciplinesReference.1.title',
+      discipline2: 'disciplinesReference.2.title',
+      discipline3: 'disciplinesReference.3.title',
+    },
+    prepare(selection) {
+      const {title, discipline0, discipline1, discipline2, discipline3, thumb} = selection
+      const disciplines = [discipline0, discipline1, discipline2, discipline3].filter(Boolean)
+      const subtitle = disciplines.length > 0 ? `${disciplines.join(', ')}` : ''
+      return {
+        title: title,
+        subtitle: subtitle,
+        media: thumb ? thumb : RiGitCommitLine
+      }
+    }
+  }
+}
