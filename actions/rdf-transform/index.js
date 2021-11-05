60741977245a04f17ba9e26cfadf1a3da366ca36
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-11-05 07:46:39 -0700

    Add rdfFormat funciton

--- actions/rdf-transform/index.js
+++ actions/rdf-transform/index.js
@@ -1,5 +1,35 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs').promises;
+
+async function rdfFormat(sanityData) {
+  try {
+    // read data
+    let data = await fs.readFile(sanityData);
+    // parse to JSON
+    let jData = JSON.parse(data);
+    // create string variable to hold triples
+    let rdfData = '';
+    // build list with an IRI for each method title
+    jData.result.forEach(method => {
+        // assign current method in loop
+        let currentMethod = `<https://uxmethods.org/${method.methodId}>`;
+        // if there are inputs, write them out as triples
+        method.inputs && method.inputs.forEach(input => {
+          rdfData += `${currentMethod} <https://uxmethods.org/ontology/hasInput> <https://uxmethods.org/${input.inputId}>.\n`;
+        });
+        // if there are outputs, write them out as triples
+        method.outputs && method.outputs.forEach(output => {
+          rdfData += `${currentMethod} <https://uxmethods.org/ontology/hasOutput> <https://uxmethods.org/${output.outputId}>.\n`;
+        });
+    });
+    // write triples string variable to .ttl document
+    await fs.writeFile('rdfData.ttl', rdfData);
+  } catch (error) {
+    // report any errors in the process
+    console.error(`Got an error trying to convert to RDF: ${error.message}`);
+  }
+}
 
 try {
   // Test input defined in action.yml metadata file
@@ -11,14 +41,16 @@ try {
     const sanityDataOutput = JSON.stringify(sanityData, null, 2);
     console.log(`Here is that data again ${sanityData}`);
 
-    // const fs = require('fs');
-    // const data = "This is the new content of the file.";
-    // fs.writeFile('createdFile.txt', data, (err) => {
-    //     if(err) {
-    //         throw err;
-    //     }
-    //     console.log("Data has been written to file successfully.");
-    // });
+  rdfFormat(sanityData);
+
+  // const fs = require('fs');
+  // const data = "This is the new content of the file.";
+  // fs.writeFile('createdFile.txt', data, (err) => {
+  //     if(err) {
+  //         throw err;
+  //     }
+  //     console.log("Data has been written to file successfully.");
+  // });
 
 } catch (error) {
   core.setFailed(error.message);
