3cb778f031e3d6e3951de5b7d036b3dacdb3eb9b
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-11-18 13:04:21 -0800

    Convert nav .js in batsby-browser to useEffect() in Layout

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,10 +1,32 @@
-import * as React from 'react'
+import React, { useEffect} from 'react'
 import SideNav from './sideNav'
 import Header from './header'
 import Footer from './footer'
-import * as s from './layout.module.scss'
 
 const Layout = ({ children }) => {
+
+  useEffect(() => {
+    // Mobile Nav
+    // Open and close mobile menu
+    document.getElementById("openMenu").addEventListener("click", function () {
+      document.getElementById("globalNav").style.transform = "translateX(0)";
+      // document.getElementById("navint").classList.add("open")
+    });
+    document.getElementById("closeMenu").addEventListener("click", function () {
+      document.getElementById("globalNav").style.transform = "translateX(100%)";
+    //   document.getElementById("navint").classList.remove("open")
+    });
+
+    // Mobile Nav Dropdowns
+    var dropdowns = document.getElementsByClassName("dropdown-toggle");
+    for (var i = 0; i < dropdowns.length; i++) {
+        dropdowns[i].addEventListener('click', function () {this.classList.toggle("show")});
+    };
+
+    // Consider switching this over to useState() for cleaner integration:
+    // https://medium.com/skillthrive/build-a-react-accordion-component-from-scratch-using-react-hooks-a71d3d91324b
+  });
+
   return (
     <>
       <SideNav />
