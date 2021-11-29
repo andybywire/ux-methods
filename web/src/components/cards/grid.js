d07c5c2dbae31aaa5b8e97a61e74a1d5e61e88c8
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-11-29 13:55:33 -0800

    Consolidate & streamline grid component & grid styles

--- web/src/components/cards/grid.js
+++ web/src/components/cards/grid.js
@@ -1,10 +1,10 @@
-import React from "react"
-import * as s from "./grid.module.scss"
+import React from 'react';
+import * as s from './grid.module.scss';
 
 const Grid = ({ children, style }) => {
 
   const gridStyle =
-    (style === "dark") ? [s.grid, s.dark].join(' ') : s.grid;
+    (style === 'dark') ? [s.grid, s.dark].join(' ') : s.grid;
 
   return(
     <ul className={gridStyle}>
