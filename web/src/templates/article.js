fc695ce1aeb578641e8b0c40f34c58207cf98a1f
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-12-01 14:27:03 -0800

    Consolidate semantics and style of article page component.

--- web/src/templates/article.js
+++ web/src/templates/article.js
@@ -1,18 +1,17 @@
-import React from "react"
-import { graphql } from "gatsby"
-import * as s from "./article.module.scss"
+import React from 'react'
+import { graphql } from 'gatsby'
+import * as s from './article.module.scss'
 import Layout from '../components/layout'
-import SanityImage from "gatsby-plugin-sanity-image"
-import PortableText from "../components/portableText"
+import SanityImage from 'gatsby-plugin-sanity-image'
+import PortableText from '../components/portableText'
 
 export default function MethodPage({data:{article}}) {
 
   return (
     <Layout>
-      <article>
-        <section className={s.article}>
+      <article className={s.article} >
           <div className={s.hero}>
-            <SanityImage {...article.heroImage} width={500} alt=""/>
+            <SanityImage {...article.heroImage} width={500} alt=''/>
           </div>
           <div className={s.header}>
             <h1>{article.title}</h1>
@@ -20,7 +19,6 @@ export default function MethodPage({data:{article}}) {
           <div className={s.body}>
             <PortableText blocks={article.body} />
           </div>
-        </section>
       </article>
     </Layout>
   )
