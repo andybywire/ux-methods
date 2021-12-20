5af8a385b94ab9177ed21ccc368ad94d3d8f3113
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2021-12-20 11:07:44 -0800

    Add basic phpmail test script

---
+++ web/static/api/contact-test/index.php
@@ -0,0 +1,4 @@
+<?php
+//sending email with the php mail()
+mail('andy@andyfitzgeraldconsulting.com', 'Subject Line For Really basic test message', 'Body of suuuuper basic test message.', 'From: contact@dev.uxmethods.org');
+?>
