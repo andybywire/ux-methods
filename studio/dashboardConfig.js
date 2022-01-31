22d730eccabebfbcb02176891619d877983c3e08
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2022-01-31 15:19:47 -0800

    Add CI badges to README

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,25 @@
+export default {
+  widgets: [
+    {
+      name: 'project-info',
+      layout: {
+        width: 'small',
+        height: 'medium'
+      }
+    },
+    {
+      name: 'project-users',
+      layout: {
+        width: 'small',
+        height: 'medium'
+      }
+    },
+    {
+      name: 'cats',
+      layout: {
+        width: 'small',
+        height: 'medium'
+      }
+    }
+  ]
+}
