1a1cc2553033d0a48ea7da1e9a8bacdf67c64154
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-02-01 17:31:29 -0800

    Add Taxonomy Manager plugin

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,7 +1,12 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {RiSettings4Line} from 'react-icons/ri'
 import {visionTool} from '@sanity/vision'
+import {taxonomyManager} from 'sanity-plugin-taxonomy-manager'
 import {schemaTypes} from './schemaTypes'
+
+const hiddenDocTypes = (listItem: any) =>
+  !['siteSettings', 'skosConcept', 'skosConceptScheme'].includes(listItem.getId())
 
 export default defineConfig({
   name: 'default',
@@ -10,9 +15,35 @@ export default defineConfig({
   projectId: '4g5tw1k0',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure: (S) => {
+        return S.list()
+          .title('UX Methods')
+          .items([
+            ...S.documentTypeListItems().filter(hiddenDocTypes),
+            S.divider(),
+            S.listItem()
+              .title('Settings')
+              .icon(RiSettings4Line)
+              .child(S.document().schemaType('siteSettings').documentId('siteSettings')),
+          ])
+      },
+    }),
+    visionTool(),
+    taxonomyManager({
+      baseUri: 'https://uxmethods.org/',
+    }),
+  ],
 
   schema: {
     types: schemaTypes,
   },
+  document: {
+    comments: {
+      enabled: false,
+    },
+  },
+  tasks: {enabled: false},
+  scheduledPublishing: {enabled: false},
 })
