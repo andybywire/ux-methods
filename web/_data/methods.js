f91ee9a4f43e64299c69a20e2ec8f40d2cd6eb21
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-02-14 06:34:06 -0800

    Add Method template and data
    - sketch out some page sections
    - streamline semantic tags

---
+++ web/_data/methods.js
@@ -0,0 +1,38 @@
+import {client} from '../utils/sanityClient.js'
+import {toHTML} from '@portabletext/to-html'
+import groq from 'groq'
+
+// Map serializers to Portable Text components
+function prepareMethod(method) {
+  return {
+    ...method,
+    overview: toHTML(method.overview),
+    steps: toHTML(method.steps),
+    // lede: toHTML(article.lede, { components: afcComponents }),
+  }
+}
+
+// Fetch methods from Sanity & prepare them for Eleventy
+async function getMethods() {
+  const methods = await client.fetch(groq`
+    *[_type == "method"] | order(title) {
+      title,
+      "slug": slug.current,
+      "uri": uri.current,
+      "createdAt": dateStamp.createdAt,
+      "revisedAt": dateStamp.revisedAt,
+      metaDescription,
+      "heroImage": {
+        "caption": heroImage.caption,
+        "altText": heroImage.alt,
+        "url": heroImage.asset->url,
+      },
+      overview,
+      steps,
+    }
+  `)
+  const preparedMethods = methods.map(prepareMethod)
+  return preparedMethods
+}
+
+export default getMethods
