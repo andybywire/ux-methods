51d7edbda68d1354d42e85886535758aba102c10
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-02-16 08:01:36 -0800

    Add method previous and next method references

--- web/_data/methods.js
+++ web/_data/methods.js
@@ -4,92 +4,68 @@ import groq from 'groq'
 import {readFileSync} from 'fs'
 import {join} from 'path'
 
-// Map serializers to Portable Text components
-function prepareMethod(method) {
-  return {
-    ...method,
-    overview: toHTML(method.overview),
-    steps: toHTML(method.steps),
-    // lede: toHTML(article.lede, { components: afcComponents }),
-  }
-}
-
-// ############################################
-
-// filter `methodPreviews` to include only those methods that are a "destination" for which the current method is an "origin" in the sharedOutput.json object
-// add the filtered `methodPreviews` to the current method object as `nextMethods`
-
-
-// load the shared-output.csv 
-const sharedOutputString = readFileSync(join(process.cwd(), '_data/kg-data/shared-output.csv'), 'utf-8')
-
-// load the shared-output.csv as a json object
-const sharedOutput = sharedOutputString.split('\n').map(row => {
-  const [origin, destination, sharedOutputCount] = row.split(',')
-  return {origin, destination, sharedOutputCount}
-})
-
-// output example:
-// {
-//   origin: 'https://uxmethods.org/method/StakeholderInterviewing',
-//   destination: 'https://uxmethods.org/method/JourneyMapping',
-//   sharedOutputCount: '3'
-// },
-
-
-
-//  loop through the methodPreviews object and filter to include only those methods that are among the destination methods fo the provided origin method.
-function getRelatedInputs(uri, sharedOutput) {
-  const relatedInputs = sharedOutput.map(relationship => relationship.origin === uri)
-  return relatedInputs
-}
-
-
-
-function getInputPreviews(methodPreviews, relatedInputs) {
-  const inputPreviews = methodPreviews.filter(method => {
-    return relatedInputs.includes(method.uri)
+// Load shared-output.csv
+// This is a stopgap solution until the KG is back online, but we'll use a cached version
+// as a backup going forward which could be something like this.
+const sharedOutput = readFileSync(join(process.cwd(), '_data/kg-data/shared-output.csv'), 'utf-8')
+  .split('\n')
+  .map((row) => {
+    const [origin, destination, sharedOutputCount] = row.split(',')
+    return {origin, destination, sharedOutputCount}
   })
-  return inputPreviews
-} 
 
-
-
-function getRelatedMethods(methodPreviews, sharedOutput) {
-  const relatedMethods = methodPreviews.map(output => {
-    return sharedOutput.find(row => row.origin === method.uri && row.destination === output.uri)
+function prepareMethod(methods, methodPreviews) {
+  const preparedMethods = methods.map((method) => {
+    // Filter and assign methods preview list to previous methods
+    const previousMethods = methodPreviews.filter((methodPreview) => {
+      return sharedOutput.some((relationship) => {
+        return relationship.origin === method.uri && relationship.destination === methodPreview.uri
+      })
+    })
+    // Filter and assign methods preview list to next methods
+    const nextMethods = methodPreviews.filter((methodPreview) => {
+      return sharedOutput.some((relationship) => {
+        return relationship.origin === method.uri && relationship.destination === methodPreview.uri
+      })
+    })
+    // Map serializers to Portable Text components and previous and next lists
+    return {
+      ...method,
+      overview: toHTML(method.overview),
+      steps: toHTML(method.steps),
+      prepareMethods: previousMethods,
+      continueMethods: nextMethods,
+      // lede: toHTML(article.lede, { components: afcComponents }), // reminder for how components are added
+    }
   })
-  return relatedMethods
+  return preparedMethods
 }
-
-
-// ############################################
 
 // Fetch methods from Sanity & prepare them for Eleventy
 async function getMethods() {
   const methods = await client.fetch(groq`
     *[_type == "method"] | order(title) {
-      title,
-      "slug": slug.current,
-      "uri": uri.current,
-      "createdAt": dateStamp.createdAt,
-      "revisedAt": dateStamp.revisedAt,
-      metaDescription,
-      "heroImage": {
-        "caption": heroImage.caption,
-        "altText": heroImage.alt,
-        "url": heroImage.asset->url,
-      },
-      overview,
-      steps,
-      "outcomes": output[]->{
-        prefLabel,
-        definition,
-      }, 
-    }
+        title,
+        "slug": slug.current,
+        "uri": uri.current,
+        "createdAt": dateStamp.createdAt,
+        "revisedAt": dateStamp.revisedAt,
+        metaDescription,
+        "heroImage": {
+          "caption": heroImage.caption,
+          "altText": heroImage.alt,
+          "url": heroImage.asset->url,
+        },
+        overview,
+        steps,
+        "outcomes": output[]->{
+          prefLabel,
+          definition,
+        }, 
+      }
   `)
-
-  const methodPreviews = methods.map(method => {
+  // Prepare previews of all methods for previous and next methods sections
+  const methodPreviews = methods.map((method) => {
     return {
       title: method.title,
       slug: method.slug,
@@ -97,8 +73,7 @@ async function getMethods() {
       metaDescription: method.metaDescription,
     }
   })
-  
-  const preparedMethods = methods.map(prepareMethod)
+  const preparedMethods = prepareMethod(methods, methodPreviews)
   return preparedMethods
 }
 
