82176e8d2ef4c68ba3f38b520bbd2c2931e0495c
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-10-09 11:00:21 -0700

    Set tsconfig to use either .yalc or node_modules for taxonomy manager plugin (for testing)

--- studio/sanity.config.tsx
+++ studio/sanity.config.tsx
@@ -4,7 +4,7 @@ import {structureTool} from 'sanity/structure'
 import {RiSettings4Line} from 'react-icons/ri'
 import type {StructureBuilder} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
-import {taxonomyManager} from './.yalc/sanity-plugin-taxonomy-manager' // for local development
+import { taxonomyManager  } from 'sanity-plugin-taxonomy-manager' // will load from .yalc if available, from node_modules if not
 import {schemaTypes} from './schemaTypes'
 import {embeddingsIndexDashboard, embeddingsIndexReferenceInput} from '@sanity/embeddings-index-ui'
 import {assist} from '@sanity/assist'
