fa473dbf83fef125b417facff13c67980da626d5
Andy Fitzgerald <andy@andyfitzgeraldconsulting.com>
2025-11-09 12:01:23 -0800

    Configure vite for typegen watch

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,16 +1,16 @@
-// @ts-check
-import {defineConfig} from "astro/config";
-
-import sanity from "@sanity/astro";
-import react from "@astrojs/react";
+import {defineConfig} from 'astro/config'
+import sanity from '@sanity/astro'
+import react from '@astrojs/react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import {watchAndRun} from 'vite-plugin-watch-and-run'
 
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: "4g5tw1k0",
-      dataset: "production",
+      projectId: '4g5tw1k0',
+      dataset: 'production',
       useCdn: false,
-      apiVersion: "2025-11-01",
+      apiVersion: '2025-11-01',
       // studioBasePath: "/studio", // If you want to access the Studio on a route
       // stega: {
       //   studioUrl: "/studio",
@@ -19,4 +19,15 @@ export default defineConfig({
     react(),
   ],
   // output: 'server', // this should be the only line I need to change for the `preview` subdomain
-});
+  vite: {
+    plugins: [
+      tsconfigPaths(),
+      watchAndRun([
+        {
+          watch: ['src/sanity/sanity.queries.ts'],
+          run: 'pnpm run gen:types',
+        },
+      ]),
+    ],
+  },
+})
